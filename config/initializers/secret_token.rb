a7600c3d33d4844ca51740955cea820f88517ba8
Tum Chaturapruek <Tum_Chaturapruek@HMC.Edu>
2012-10-27 16:35:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuddSD::Application.config.secret_token = 'a8dfa274d5f64bc1d6b6e0496b6de2529d94cd98d5c89be786367b12e947d05bcdc9ffacc2b49703b594a67744da00fcbd810f162787e259791919e5e2b2a916'
