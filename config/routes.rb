acf98e61ae4404466c70712ac9964f0efc0df4af
Tum Chaturapruek <Tum_Chaturapruek@HMC.Edu>
2012-10-27 17:23:26 -0700

    add Projects page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ MuddSD::Application.routes.draw do
 
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
+  match '/projects', to: 'static_pages#projects'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',  to: 'users#new'
